ce07a36473c711005834ca9459305deb600f2a79
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-05-04 00:27:32 +0530

    Add files via upload
    Learned fundamental concepts in C programming, specifically focusing on dynamic memory allocation using `malloc` and file input/output (I/O) operations. The experience included hands-on practice in programmatically creating, writing to, opening, and deleting files. The learning was quite effective and insightful.

---
+++ read_file.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main ()
+{
+    int fd = 0;
+    int Ret = 0;
+    char Arr[20];
+
+    fd = open("Marvellous.txt",O_RDWR);
+
+    if(fd==-1)
+    {
+        printf("Unable to open file");
+    }
+    else 
+    {
+        printf("File gets opened with fd : %d\n",fd);
+        Ret = read(fd,Arr,6);
+
+        printf("%d bytes get read from the file\n",Ret);
+
+        printf("%s\n",Arr);
+    }
+    close (fd);
+ 
+    return 0;
+}
